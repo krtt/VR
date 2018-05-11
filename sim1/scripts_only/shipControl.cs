6ffbfb164ac4d1c834ada95a9fee6ef0aa1a47af
krtt <tokertu@gmail.com>
2018-05-12 01:14:40 +0300

    added pitch

--- sim1/scripts_only/shipControl.cs
+++ sim1/scripts_only/shipControl.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class shipControl : MonoBehaviour {
 
-    /*
-    float rollSpeed = 100.0f; // rolling sideways
-    float pitchSpeed = 100.0f; // flip back/forth
-    float yawSpeed = 100.0f; // turn left/right
-    */
     Rigidbody rb;
     public float linearForceAmount = -1.0f;
     public float mass = 1; // unity tut
@@ -25,85 +20,30 @@ public class shipControl : MonoBehaviour {
 
 		// 8th, AddForce + AddTorque try:
 
-		float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+		float h = Input.GetAxis("Horizontal"); // for yaw
+        float v = Input.GetAxis("Vertical"); // for roll
+        float p = Input.GetAxis("Pitch");
 
+        // meaning: transform.forward is a shortcut to forward-directional axis (Z)
         forceVect = transform.forward * linearForceAmount * v;
         
         // usage: AddForce(float x, float y, float z, ForceMode mode = ForceMode.Force)
         rb.AddForce(forceVect);
 
+        // meaning: h for rotating around up-axis (Y)
         rb.AddTorque(transform.up * trqForce * h);
+
+        // meaning: p for rotating around right-axis (X) >> pitch | use 5th axis in Unity InputManager !!!
+        rb.AddTorque(transform.right * trqForce * p);
         
         /* Notes for working values for above (blue ship works. black falls down) :
          * forward movement with joystick ok
          * turning left-right with joystick ok 
+         * pitch with joystick ok (found 5th axis, then working ok)
          * blue ship params: | rb: mass 1, drag 1, angulardrag 1, usegravity none | public: linearforceamount -1, mass 1, torqueforce 0.1 
          * black ship params: | rb: mass 1, drag 0, angulardrag 0.05, usegravity yes | public: linearforceamount -1, mass 1, torqueforce 0.1
          * */
-
-
-
-
-
-
-        //		// 4th, rotation while moving (but needs AddForce as I don't want to disable gravity):
-        //		rb.AddForce (0, 9.81f, 0); // constant floating
-        //		// use addForce instead:
-        //
-        //
-        //		// bypasses physics:
-        //		transform.position += transform.forward * Time.deltaTime; // * speed; // very slow if no speed (ok)
-        //		transform.Rotate(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal")); // rotation only
-
-
-
-        // 3rd, simple rotation:
-        //		if(Input.GetKeyDown(KeyCode.LeftArrow)){
-        //			transform.Rotate (0, -90.0f, 0); // turns 90 deg immediately
-        //		}
-
-
-
-        // 2nd, simple x, y movement while floating:
-
-        //		rb.AddForce (0, 9.81f, 0); // constant floating
-        //		float horizontal = Input.GetAxis ("Horizontal");
-        //		float vertical = Input.GetAxis ("Vertical"); // It's not vertical! It's forward/backward.
-        ////		float upDn = Input.GetAxis("???"); // what can i use here for 3rd axis so i can avoid using GetKey... and transform.Translate ?
-        //		Vector3 movement = new Vector3 (horizontal * speed * Time.deltaTime, 0, vertical * speed * Time.deltaTime);
-        //		rb.MovePosition (transform.position + movement);
-
-
-
-        // 1st try, simple up, down, left, right with arrow keys:
-
-        //		rb.AddForce (0, 9.81f, 0); // constant floating
-        //
-        //		if (Input.GetKey("right")) {
-        //			transform.Translate (0.1f, 0, 0);
-        //		}
-        //		if (Input.GetKey("left")) {
-        //			transform.Translate (-0.1f, 0, 0);
-        //		}
-        //		if (Input.GetKey("up")) {
-        //			transform.Translate (0, 0, 0.1f);
-        //		}
-        //		if (Input.GetKey("down")) {
-        //			transform.Translate (0, 0, -0.1f);
-        //		}
-        //		if (Input.GetKey("w")) {
-        //			transform.Translate (0, 0.1f, 0);
-        //		}
-        //		if (Input.GetKey("z")) {
-        //			transform.Translate (0, -0.1f, 0);
-        //		}
-        //
-        //		 Something that I don't want:
-        //
-        //		if (Input.GetKey(KeyCode.Space)){
-        //			rb.AddForce (0, 10.8f, 0); // going up when holding space - not exactly ok gravity usage
-        //		}
+         
     }
 }
 
