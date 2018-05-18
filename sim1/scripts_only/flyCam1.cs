8e2fb169a29cef5074db608bbf22d4354a1887e5
krtt <tokertu@gmail.com>
2018-05-19 01:05:45 +0300

    link to source added

---
+++ sim1/scripts_only/flyCam1.cs
@@ -0,0 +1,29 @@
+﻿// source: https://forum.unity.com/threads/fly-cam-simple-cam-script.67042/
+
+// this does not work:
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flyCam : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+
+    void LateUpdate()
+    {
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor); 
+    }
+}
